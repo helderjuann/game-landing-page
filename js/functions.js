c4c5bdd2b2821f0bf9e19fd90c905cce923d94c6
Heldinnn <124900375+helderjuann@users.noreply.github.com>
2023-09-13 18:27:42 -0300

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,11 @@
 $(function() {
-	
+    disableTextS();
+
+    function disableTextS() {
+        $('body').css('-webkit-user-select','none');
+        $('body').css('-moz-user-select','none');
+        $('body').css('-ms-user-select','none');
+        $('body').css('-o-user-select','none');
+        $('body').css('user-select','none');
+    }
 })
